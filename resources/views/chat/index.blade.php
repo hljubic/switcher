1fed8d447c1560c6baa535bb15a7c5f8e67cb4fb
marin <marin94bosnjakc2@gmail.com>
2017-12-07 16:32:27 +0100

    Merge branch 'master' of D:\xampp\htdocs\switcher with conflicts.

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -1,167 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="lista-razgovora col-lg-3">
 
-        <h2 class="message-list-title">Lista razgovora</h2>
-        <div class="message-list-search" ng-controller="search">
-
-            <input class="search-input" type="text" ng-model="how" placeholder="Search">
-
-            <span class="search-icon"></span>
-        </div>
-
-
-        <ul class="message-items">
-
-
-            @foreach($conversations as $conversation)
-                <li class="list-group-item active btn-default">
-                    <a href="{{route("messages",["conversation_id" => $conversation->id])}}">
-                        <div class="message-user-icon"></div>
-                        <div class="message-user-name">{{ $conversation->user->name }}</div>
-                        <br><br>
-                        <div class="message-user-status help-block">{{ $conversation->title }}</div>
-                    </a>
-                </li>
-        @endforeach
-
-        <!--
-            <li class="list-group-item active btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Slaven Butigan</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Ivan Kordić</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-            <li class="list-group-item btn-default">
-                <div class="message-user-icon"></div>
-                <div class="message-user-name">Marin Bošnjak</div>
-                <br><br>
-                <div class="message-user-status help-block">Active 2 m ago</div>
-            </li>
-        -->
-
-        </ul>
-
-    </div>
-    <!--
-    <div class="container">
+    <div class="container-fluid" ng-controller="MainCtrl">
         <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Conversation</div>
-                    <div class="panel-body">
+            <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+                <ul class="nav nav-pills flex-column">
+
+                    <h3 class="conversations-label">Conversations</h3>
+
+                    <li class="nav-item">
+                        <!-- dodaje klasu active -->
+                        <a class="nav-link active" href="#"><% current_conv %><span class="sr-only">(current)</span></a>
+                    </li>
+
+                    <li class="nav-item" ng-repeat="user in users track by $index">
+                        <a class="nav-link" href="#" ng-click="change_conv()"><% user %></a>
+                    </li>
+
+                </ul>
+            </nav>
+
+            <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
+                <h3>Dashboard</h3>
+                <hr>
+
+
+
+                <div class="media mb-3" ng-repeat="message in messages track by $index">
+
+                    <div class="media-body" ng-if="message.creator_id == 2 && message.conversation_id == current">
+                        <img class="mr-4 swt-no-break-img" src="https://cdn.jsdelivr.net/emojione/assets/png/1f439.png" alt="Generic placeholder image">
                         <div class="row">
-                            <div class="col-md-6">
-                                <form>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="message" placeholder="Message">
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-primary form-control">Send</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="col-md-6">
-                                <ul id="messages">
-                                </ul>
+                            <div class="col-3 bg-dark text-white swt-message-rounded">
+
+                                <p class="font-weight-bold"><% message.content %></p>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
--->
-    <div class="col-lg-7">
-        <div class="list-messagers">
-            <div class="user-header">
 
-                <div class="user-name">{{Auth::user()->name}}</div>
 
-                <br><br>
-                <div class="user-status help-block">Active 2 m ago</div>
-            </div>
-            <!-- prvi pošiljatelj -->
-            <div class="users-messages">
-                <ul id="poruke">
-                    @foreach($messages as $message)
-                        <li class="@if ($message->user->id == Auth::user()->id) {{"me"}} @else {{"you"}}@endif">
-                            <div class='list-group-item'>
-                                {{ $message->user->name}}
-                                <div class="message-time help-block">
-                                    {{ $message->created_at }}
-                                </div>
-                                <div class="messager-right div @if ($message->user->id == Auth::user()->id) {{"left"}} @else {{"right"}}@endif">
-                                    {{ $message->content }}
-                                </div>
-                                <div class="clear"></div>
+                    <div class="media-body" ng-if="message.sender_id != 2 && message.conversation_id == current">
+                        <div class="row">
+                            <div class="col-8"></div>
+                            <div class="col-3 bg-info text-white swt-message-rounded">
+                                <p class="font-weight-bold text-right"><% message.content %></p>
                             </div>
+                            <div class="col-1">
+                                <img class="align-self-start mr-4" src="https://cdn.jsdelivr.net/emojione/assets/png/1f439.png" alt="Generic placeholder image">
+                            </div>
+                        </div>
+                    </div>
 
-                        </li>
-                    @endforeach
-                </ul>
-
-            </div>
-
-            <p id="typing" style="display:none;">Typing...</p>
-
-
-            <form>
-                {{csrf_field()}}
-                <div class="input-messager">
-                    <input id="sender_id" type="hidden" value="{{Auth::user()->id}}">
-                    <input id="conversation_id" type="hidden" value="{{$conversation_id}}">
-                    <input id="m" type="text" name="chat" class="form-control" placeholder="Unesite poruku...."></input>
-                    <br><br>
-                    <button id="submit" type="submit" class="btn btn-default">SEND</button>
                 </div>
-            </form>
+            </main>
         </div>
-
     </div>
+
 @endsection
 
 @push('scripts')
@@ -169,75 +64,68 @@
     <script src="{{asset('https://code.jquery.com/jquery-1.11.1.js')}}"></script>
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    <script src="http://localhost:3000/socket.io/socket.io.js"></script>
+    <script src="{{asset('js/ang_app.js')}}"></script>
+    {{--<script src="http://localhost:3000/socket.io/socket.io.js"></script>--}}
 
 
     <script>
-
-        //** ANGULAR **///
-
-
-        var app = angular.module('switcher', []);
-        app.controller('MainCtrl', function ($scope) {
-            $scope.name = "boze"
-            // alert("aaaaa")
-        });
+        <!-- ANGULAR -->
 
 
+        <!-- kraj angulara-->
 
-
-        $(function () {
-            var socket = io('localhost:3000');
-            $('form').submit(function () {
-                socket.emit('send_message', $('#m').val());
-                var date = {
-                    'content': $('#m').val(),
-                    'conversation_id': $('#conversation_id').val(),
-                    'sender_id': $('#sender_id').val(),
-                    'created_at': $('#created_at').val()
-                };
-                $.ajax({
-                    type: "POST",
-                    url: "{{route("message_create")}}",
-                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                    data: date
-                });
-                $('#m').val('');
-                //socket.emit('typing', 0);
-                return false;
-            });
-            $('#m').on('keyup', function () {
-                if (($('#m').val() === '')) {
-                    socket.emit('typing', 0);
-                } else {
-                    socket.emit('typing', 1);
-                }
-            });
-            socket.on('typing', function (msg) {
-                if (msg === 1) {
-                    $('#typing').show();
-                }
-                else {
-                    $('#typing').hide();
-                }
-            });
-            socket.on('receive_message', function (msg) {
-                var date = new Date();
-                var nova_poruka = "<li class=" + 'me' + " id=" + 'id' + " >" +
-                    //"<div class=\'message-user1-icon\'></div>"+
-                    "<div class='list-group-item' >" + '{{ Auth::user()->name }}' +
-                    "<div class=\"message-time help-block\">" + date.getYear() + "-" + date.getMonth() + "-" + date.getDay() + " " + date.getHours() + ':' + date.getMinutes() +
-                    "</div>" +
-                    "<div  class='messager-right div left'>" + msg +
-                    "</div>" +
-                    "<div class='clear'></div>" +
-                    "</div>" +
-                    //'<p>' + msg + '</p>' +
-                    "</li>";
-                // nova_poruka.find('p').text(msg);
-                $('#poruke').append(nova_poruka);
-                //$('#poruke').append($('<li>').html('{{ Auth::user()->name }} at ' + date.getHours() + ':' + date.getMinutes() + ' sent : <strong>' + msg + '</strong>'));
-            });
-        });
+        {{--$(function () {--}}
+            {{--var socket = io('localhost:3000');--}}
+            {{--$('form').submit(function () {--}}
+                {{--socket.emit('send_message', $('#m').val());--}}
+                {{--var date = {--}}
+                    {{--'content': $('#m').val(),--}}
+                    {{--'conversation_id': $('#conversation_id').val(),--}}
+                    {{--'sender_id': $('#sender_id').val(),--}}
+                    {{--'created_at': $('#created_at').val()--}}
+                {{--};--}}
+                {{--$.ajax({--}}
+                    {{--type: "POST",--}}
+                    {{--url: "{{route("message_create")}}",--}}
+                    {{--headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},--}}
+                    {{--data: date--}}
+                {{--});--}}
+                {{--$('#m').val('');--}}
+                {{--//socket.emit('typing', 0);--}}
+                {{--return false;--}}
+            {{--});--}}
+            {{--$('#m').on('keyup', function () {--}}
+                {{--if (($('#m').val() === '')) {--}}
+                    {{--socket.emit('typing', 0);--}}
+                {{--} else {--}}
+                    {{--socket.emit('typing', 1);--}}
+                {{--}--}}
+            {{--});--}}
+            {{--socket.on('typing', function (msg) {--}}
+                {{--if (msg === 1) {--}}
+                    {{--$('#typing').show();--}}
+                {{--}--}}
+                {{--else {--}}
+                    {{--$('#typing').hide();--}}
+                {{--}--}}
+            {{--});--}}
+            {{--socket.on('receive_message', function (msg) {--}}
+                {{--var date = new Date();--}}
+                {{--var nova_poruka = "<li class=" + 'me' + " id=" + 'id' + " >" +--}}
+                    {{--//"<div class=\'message-user1-icon\'></div>"+--}}
+                    {{--"<div class='list-group-item' >" + '{{ Auth::user()->name }}' +--}}
+                    {{--"<div class=\"message-time help-block\">" + date.getYear() + "-" + date.getMonth() + "-" + date.getDay() + " " + date.getHours() + ':' + date.getMinutes() +--}}
+                    {{--"</div>" +--}}
+                    {{--"<div  class='messager-right div left'>" + msg +--}}
+                    {{--"</div>" +--}}
+                    {{--"<div class='clear'></div>" +--}}
+                    {{--"</div>" +--}}
+                    {{--//'<p>' + msg + '</p>' +--}}
+                    {{--"</li>";--}}
+                {{--// nova_poruka.find('p').text(msg);--}}
+                {{--$('#poruke').append(nova_poruka);--}}
+                {{--//$('#poruke').append($('<li>').html('{{ Auth::user()->name }} at ' + date.getHours() + ':' + date.getMinutes() + ' sent : <strong>' + msg + '</strong>'));--}}
+            {{--});--}}
+        {{--});--}}
     </script>
 @endpush
